f4a76249d6998d4fe309fea4d3f8e0b161b76479
Manuel rincon <manuelrincon37@gmail.com>
2023-07-29 17:34:30 -0400

    save albun method update

--- routes/album.js
+++ routes/album.js
@@ -1,10 +1,12 @@
 //Import dependencies
 const express = require("express")
+const check = require("../middleware/auth")
 //Load router
 const router = express.Router()
 //Import controller
 const albumController = require("../controllers/album")
 //Define route
 router.get("/album-test", albumController.test)
+router.post("/save", check.auth, albumController.save)
 //Export routes
 module.exports = router
